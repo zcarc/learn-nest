38beccfc9ca255d62165386b5a6d2ea43bc65df6
HyunsooLee <wrkbrs@gmail.com>
2021-01-07 15:24:49 +0900

    service

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -8,7 +8,6 @@ import {
   Param,
   Patch,
   Post,
-  Query,
 } from '@nestjs/common';
 
 @Controller('movies')
--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,9 +1,9 @@
 import { Movie } from './entities/movie.entity';
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 
 @Injectable()
 export class MoviesService {
-  private movies: Movie[];
+  private movies: Movie[] = [];
 
   // 모든 영화를 가져옴
   getAll(): Movie[] {
@@ -12,13 +12,16 @@ export class MoviesService {
 
   // 영화 검색 기존에 있었던 movie.id(number)을 파라미터(id: string) to numberㄹ로 변환
   getOne(id: string): Movie {
-    return this.movies.find((movie) => movie.id === +id);
+    const movie = this.movies.find((movie) => movie.id === +id);
+    if (!movie) {
+      throw new NotFoundException(`Movie with ID ${id} not found.`);
+    }
+    return movie;
   }
 
   // 영화 삭제 해당 아이디와 일치하지 않는 obj들만 뽑아서 다시 객체를 만듦
-  deleteOne(id: string): boolean {
-    this.movies.filter((movie) => movie.id !== +id);
-    return true;
+  deleteOne(id: string) {
+    this.movies = this.movies.filter((movie) => movie.id !== +id);
   }
 
   // 영화를 추가하고 id를 기존에 있는 영화 객체의 개수에서 +1한 값을 id로 할당함
@@ -27,5 +30,11 @@ export class MoviesService {
       id: this.movies.length + 1,
       ...movieData,
     });
+  }
+
+  update(id: string, updateData) {
+    const movie = this.getOne(id);
+    this.deleteOne(id);
+    this.movies.push({ ...movie, ...updateData });
   }
 }
