6c471917c77c7e48d777a9d9e167cfecaf7cf2aa
HyunsooLee <wrkbrs@gmail.com>
2021-04-13 14:05:59 +0900

    modify movie module

--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -14,7 +14,7 @@ export class MoviesService {
 
   // 영화 검색 기존에 있었던 movie.id(number)을 파라미터(id: string) to numberㄹ로 변환
   getOne(id: number): Movie {
-    const movie = this.movies.find((movie) => movie.id === +id);
+    const movie = this.movies.find((movie) => movie.id === id);
     if (!movie) {
       throw new NotFoundException(`Movie with ID ${id} not found.`);
     }
@@ -23,7 +23,7 @@ export class MoviesService {
 
   // 영화 삭제 해당 아이디와 일치하지 않는 obj들만 뽑아서 다시 객체를 만듦
   deleteOne(id: number) {
-    this.movies = this.movies.filter((movie) => movie.id !== +id);
+    this.movies = this.movies.filter((movie) => movie.id !== id);
   }
 
   // 영화를 추가하고 id를 기존에 있는 영화 객체의 개수에서 +1한 값을 id로 할당함
